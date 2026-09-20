d471135c92149d970f45df3c4ee6dfc6c8caa48a
AllynProgramming <allynmatt03@gmail.com>
2026-09-20 17:35:40 +0800

    reset

--- config/google-oauth.php
+++ config/google-oauth.php
@@ -4,9 +4,5 @@
 
 define('GOOGLE_CLIENT_ID', getenv('GOOGLE_CLIENT_ID') ?: '');
 define('GOOGLE_CLIENT_SECRET', getenv('GOOGLE_CLIENT_SECRET') ?: '');
-define('GOOGLE_REDIRECT_URI', getenv('GOOGLE_REDIRECT_URI') ?: 'https://gym-web-zb2q.onrender.com/api/google-callback.php');
-
-$googleClientId = getenv('GOOGLE_CLIENT_ID');
-$googleClientSecret = getenv('GOOGLE_CLIENT_SECRET');
-$redirectUri = getenv('GOOGLE_REDIRECT_URI');
+define('GOOGLE_REDIRECT_URI', 'https://progression.freedev.app/Gym-Web/api/google-callback.php');
 ?>
